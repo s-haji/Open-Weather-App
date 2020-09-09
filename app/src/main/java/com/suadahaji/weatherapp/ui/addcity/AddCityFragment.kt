92bc2b2f2e0af9a0e0f5fb0d116a6a8c71728f39
Suada Haji <suadahaji@gmail.com>
2020-09-09 11:56:53 +0300

    chore: Add Map view

--- app/src/main/java/com/suadahaji/weatherapp/ui/addcity/AddCityFragment.kt
+++ app/src/main/java/com/suadahaji/weatherapp/ui/addcity/AddCityFragment.kt
@@ -4,18 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.suadahaji.weatherapp.R
 
-class AddCityFragment : Fragment() {
+class AddCityFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    private lateinit var map: GoogleMap
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_city_detail, container, false)
+        return inflater.inflate(R.layout.fragment_add_city, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val cameraPosition = CameraPosition.Builder().target(
+            sydney
+        ).zoom(6f).build()
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        map.setOnMapClickListener(this)
+        map.uiSettings.isZoomControlsEnabled = true
+    }
+
+    override fun onMapClick(latLng: LatLng) {
+        map.addMarker(MarkerOptions().position(latLng))
+        Toast.makeText(activity, "Clicked!!", Toast.LENGTH_SHORT).show()
     }
 }
