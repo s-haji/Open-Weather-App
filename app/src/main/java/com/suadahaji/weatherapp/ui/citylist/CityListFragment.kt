fce8d000ae20b151a5f57443666ea5ab2159f641
Suada Haji <suadahaji@gmail.com>
2020-10-17 10:10:33 +0300

    chore: Add toast for delete notification

--- app/src/main/java/com/suadahaji/weatherapp/ui/citylist/CityListFragment.kt
+++ app/src/main/java/com/suadahaji/weatherapp/ui/citylist/CityListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -88,6 +89,7 @@ class CityListFragment : Fragment(), Injectable, HasAndroidInjector,
                     R.string.delete
                 ) { dialog, _ ->
                     viewModel.deleteCity(city)
+                    Toast.makeText(activity, "City deleted", Toast.LENGTH_SHORT).show()
                     dialog.dismiss()
                 }
                 setNegativeButton(
